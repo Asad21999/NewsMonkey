f486d8347d932ce9016946a6d89d07c79f63ec2d
OEM Configuration (temporary user) <asad.hussain2199@gmail.com>
2022-02-15 23:40:13 +0530

    finalll

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  
 } from "react-router-dom";
 // import NewsItem from './components/NewsItem';
 export default class App extends Component {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+   
   Link
 } from "react-router-dom";
 
